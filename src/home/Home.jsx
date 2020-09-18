1846919f1d651d27f53ab1b26124d88ae724fc99
eliftabak <elifchorghay@gmail.com>
2020-09-18 22:20:16 +0300

    action buttons

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import Navbar from "./navbar/Navbar";
+import { Button, Typography } from "@material-ui/core";
+
 
 export default function Home() {
  return (
-   <div>
-    <Navbar />
+   <div className="headerContainer">
+     <Navbar />
+     <div className="landingPageText">
+       <Typography variant="subtitle1" gutterBottom>
+         subtitle1. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+         Quos blanditiis tenetur
+       </Typography>
+       <Button variant="contained" color="secondary">
+         Layout
+       </Button>
+       <Button variant="outlined" color="secondary">
+         Learn more
+       </Button>
+     </div>
    </div>
  );
 }
