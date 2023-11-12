896a5ce400b6308c81d57f0e78ac1128a52527f0
Elif <Elif@Elifs-MacBook-Pro.local>
2023-11-12 21:15:25 +0300

    Upgrade ReactJS version to 18.2

--- src/layout/outputBar/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function OutputBar(params) {
-    return(
-        <div>
-            OutputBar
-        </div>
-    )
-}
---
+++ src/pages/About/index.tsx
@@ -0,0 +1,7 @@
+const About = () => {
+    return (
+        <div>hello</div>
+    )
+}
+
+export default About;
